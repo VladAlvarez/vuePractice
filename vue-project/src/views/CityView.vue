c4e78cf109cdd940b495fb3b31b02116067ca3b4
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-11-02 23:26:42 -0600

    first commit

---
+++ vue-project/src/views/CityView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Suspense>
+            <AsyncCityVue/>
+            <template #fallback>
+                <p>Loading...</p>
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+<script setup>
+import AsyncCityVue from '../components/AsyncCityVue.vue';
+
+
+</script>
+
